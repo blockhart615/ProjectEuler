76c47b371c23ef833d8a38ff0f7258313f8e8e86
PortableUbuntu <blockhart615@gmail.com>
2018-08-28 19:09:27 -0700

    Add solution for problem 12

--- PE_12.py
+++ PE_12.py
@@ -1,13 +1,40 @@
 # Project Euler Problem 12
 # The first triangluar number with over 500 divisors
+
+import time
+import math
+from math import sqrt
+
+
+def find_num_divisors(num):
+    '''Finds the number of divisors that the given number has'''
+    numDivisors = 0
+    root = int(math.ceil(sqrt(num)))
     
+    for i in range (1, root, 1):
+        if num % i == 0:
+            numDivisors += 2
+
+    if root * root == num:
+        numDivisors -= 1
+
+    return numDivisors
 
 
 def main():
-    sum_primes(2)
-    
+    '''main function'''
+    num = 1  # keeps track of the triangle numbers
+    counter = 2  # counter used to make triangle numbers
+
+    # loop until 
+    while find_num_divisors(num) < 500:
+        num += counter
+        counter += 1
+
+    # output results
+    print "The answer is ", num
 
 
-#Start at main function
+# Start at main function
 if __name__ == '__main__':
 	main()
